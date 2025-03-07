6421992b4468ad4cfe4060c7d8f907ba36641250
Maya Lai <mayalai@Mayas-MacBook-Air-58.local>
2025-03-07 01:13:23 -0800

    worked on code

--- game/v1-html-css-only/script.js
+++ game/v1-html-css-only/script.js
@@ -2,4 +2,24 @@
     'use strict';
     console.log('reading js');
 
+    const start = document.querySelector('#start');
+    const inst = document.querySelector('#instructions');
+    const game = document.querySelector('#game');
+
+    // Click start button to begin game
+    document.querySelector('#start-btn').addEventListener('click', function(){
+        
+        start.style.display = 'none';
+        inst.style.display = 'block';
+        game.style.display = 'none';
+    });
+
+    // The close button
+    document.querySelector('#close-btn').addEventListener('click', function(){
+        
+        start.style.display = 'none';
+        inst.style.display = 'none';
+        game.style.display = 'block';
+    });
+
 })();
